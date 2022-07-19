5ea405ff3adb508d1e04b8707054dbc0cfa36fba
usbbush <jeanbienvenusb@gmail.com>
2022-07-19 11:49:37 +0200

    Structure HTML page

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path=require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports={
-  entry: './src/index.js',
+  entry: './src/script.js',
   devServer: {
     static: './dist',
   },
