44f077be16b8b1ca8c8be6e069ad34ddf6f0dec1
usbbush <jeanbienvenusb@gmail.com>
2022-07-19 13:34:57 +0200

    Style Table

--- src/script.js
+++ src/script.js
@@ -3,7 +3,6 @@ import { tableBody } from './modules/elements';
 import generateDefaultData from './modules/data-list';
 import capitalizeString from './modules/capitalize-string';
 
-tableBody.innerHTML=generateDefaultData(6,[1,150]).map((item)=> `  <tr>
-                         <td>${capitalizeString(item.getName())}:</td>
-                         <td>${item.getScore()}</td>
+tableBody.innerHTML=generateDefaultData(7,[1,150]).map((item)=> `  <tr>
+                         <td>${capitalizeString(item.getName())} : ${item.getScore()}</td>
                      </tr>`).join('');
